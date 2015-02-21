941436449a950805b781e61181cfcf59c79b47ef
tomaszbrue <thomas@informme.de>
2015-02-21 10:24:22 +0100

    bootstraping

---
+++ traveler.cc
@@ -0,0 +1,17 @@
+//
+// traveler.cc
+// routing
+//
+// Created by tomaszbrue on 21/02/15.
+// Copyright (c) 2015 Thomas Brüggemann. All rights reserved.
+//
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, const char * argv[])
+{
+	cout << "hi" << endl;
+	return EXIT_SUCCESS;
+}
